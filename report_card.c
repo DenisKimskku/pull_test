77fefa77ef404aec426bf2bc1a29a4d489cc41fb
DenisKimskku <kor8821@naver.com>
2019-11-14 18:57:35 +0900

    Add::push push baby 1

---
+++ report_card.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+	printf("This is Report Cards.c\n");
+	printf("what is the difference ? \n");
+	printf("Math: 180 \n");
+	printf("English: 100 \n");
+	printf("Sum grade: 190 \n");
+	printf("Average grade: 95 \n");
+	printf("Push Push Baby: 1");
+}
+
